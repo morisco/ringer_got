1f5014eb350281e20b77aa7d9bc51ffba280af0b
Mike Morisco <mike.morisco@jet.com>
2017-02-21 18:58:49 -0500

    Initial build

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
         <!-- Place favicon.ico in the root directory -->
 
         <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/grid.css">
         <link rel="stylesheet" href="css/main.css">
         <script src="js/vendor/modernizr-2.8.3.min.js"></script>
     </head>
@@ -19,10 +20,55 @@
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+        <header>
+            <h1>The top 50 fast food items in america</h1>
+            <div id="filters">
+                <a href="javascript:void(0);" data-filter="All" class="active">All</a>
+                <a href="javascript:void(0);" data-filter="Fries">Fries</a>
+                <a href="javascript:void(0);" data-filter="Burger">Burgers</a>
+                <a href="javascript:void(0);" data-filter="Chicken">Chicken</a>
+                <a href="javascript:void(0);" data-filter="Chicken Sandwich">Chicken Sandwich</a>
+                <a href="javascript:void(0);" data-filter="Taco">Tacos</a>
+                <a href="javascript:void(0);" data-filter="Dessert">Dessert</a>
+                <a href="javascript:void(0);" data-filter="Misc">Misc</a>
+            </div>
+        </header>
+        <section>
+            <ul id="item-list" class="grid"></ul>
+        </section>
+
+        <script id="food-card-template" type="text/x-handlebars-template">
+            <li class="food-item col-xs-12 col-md-6">
+                <h3><span class="title">{{displayOrder}}.{{title}}</span> <span class="brand">{{brand}}</span></h3>
+                <div class="body">
+                    <div class="image-wrapper">
+                        <img src="{{img}}" alt="{{title}} - Illustration" />
+                    </div>
+                    <div class="content">
+                        {{desc}} - {{writer}}
+                        <div class="stats">
+                            <div class="stat">
+                                <span class="title">Calories.........................................................................................................................................................</span>
+                                <span class="value">750</span>
+                            </div>
+                            <div class="stat">
+                                <span class="title">Price............................................................................................................................................................</span>
+                                <span class="value">$3.50</span>
+                            </div>
+                            <div class="stat">
+                                <span class="title">First Debuted....................................................................................................................................................</span>
+                                <span class="value">1977</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </script>
 
         <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+        <script src="js/vendor/handlebars.js"></script>
+        <script src="js/vendor/underscore.js"></script>
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
     </body>
--- js/main.js
+++ js/main.js
@@ -1,39 +1,55 @@
 $(document).ready(function(){
     var masterList = new CardList();
-    var filters = new Filters();
 });
 
 var CardList = function() {
     var cardlist = this;
 
+    this.templateSource = $("#food-card-template").html();
+    this.template = Handlebars.compile(this.templateSource);
+
     this.cards = [];
 
-    this.init = function(){
+    this.init = function() {
         this.getData();
+        this.initEvents();
+    }
+
+    this.initEvents = function() {
+        $('#filters a').on('click', this.filter);
+    }
+
+    this.filter = function(){
+        var filter = $(this).data('filter');    
+        var cards = cardlist.cards.slice(0);
+        var filtered_cards = cards;
+        if(filter !== 'All') {
+            var filtered_cards = _.filter(cards, function(card) { return card.category === filter; });
+        }
+        $("#filters .active").removeClass('active');
+        $(this).addClass('active');
+        cardlist.buildList(filtered_cards);
     }
 
     this.getData = function() {
         $.getJSON( "data/cards.json", function(data) {
             cardlist.cards = data.cards;
+            cardlist.buildList(cardlist.cards);
         }) 
         .fail(function(data) {
-            console.log(arguments);
-            console.log( "error" );
+            console.log( "ERROR: Cards did not load" );
         });
     };
+
+    this.buildList = function(cards) {
+        $('#item-list').empty();
+        $.each(cards, function(index){
+            this.displayOrder = index + 1;
+            $('#item-list').append(cardlist.template(this));
+        });
+    }
 
     this.init();
 
     return cardlist;
-}
-
-var Filters = function(data) {
-    var filters = this;
-
-    filters.init = function() {
-        console.log(data);
-    };
-
-    filters.init(data);
-    return filters
 }
