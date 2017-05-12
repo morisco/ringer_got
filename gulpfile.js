8aafc6a991b499b7f21a8aae9515dfb6a53599ac
Mike Morisco <mikemorisco@gmail.com>
2017-05-11 20:46:58 -0400

    Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+
 
 gulp.task('sass', function () {
-  return gulp.src('./css/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'));
+    gulp.src('./css/*.scss')
+          .pipe(concat('all.scss'))
+          .pipe(sass().on('error', sass.logError))
+          .pipe(gulp.dest('./dist/css'));
 });
