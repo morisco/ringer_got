6cc28eadd0cb8c8eabe355ac377b8a656616d8de
Mike Morisco <mikemorisco@gmail.com>
2017-07-06 18:27:40 -0400

    More responsive adjustments

--- components/header.php
+++ components/header.php
@@ -1,22 +1,25 @@
 <header class="background-theme">
     <nav>
-        <a href="http://theringer.com" class="logo"><img src="img/logo.png" alt="The Ringer Logo" /></a>
-        <ul class="main-nav">
-            <li><a target="_blank" href="https://bit.ly/ringerhome">HOME</a></li>
-            <li><a target="_blank" href="https://bit.ly/ringernba">TV</a></li>
-            <li><a target="_blank" href="https://bit.ly/ringerncaabasketball">Binge Mode</a></li>
-            <li><a target="_blank" href="https://theringer.com/nba-draft">&lsquo;The Watch&rsquo; Podcast</a></li>
-            <li><a target="_blank" href="https://bit.ly/ringernbaplayoffs">Talk the Thrones</a></li>
-        </ul>
-        <ul class="fixed-nav">
-            <li><a target="_blank"  class="<?php echo ($sort_list_id === 'all') ? 'active_filter' : '' ?>" data-sort-id="all" href="javascript:void(0);">All Seasons</a></li>
-            <li><a target="_blank"  class="<?php echo ($sort_list_id === '1') ? 'active_filter' : '' ?>" data-sort-id="1" href="javascript:void(0);">1</a></li>
-            <li><a target="_blank"  class="<?php echo ($sort_list_id === '2') ? 'active_filter' : '' ?>" data-sort-id="2" href="javascript:void(0);">2</a></li>
-            <li><a target="_blank"  class="<?php echo ($sort_list_id === '3') ? 'active_filter' : '' ?>" data-sort-id="3" href="javascript:void(0);">3</a></li>
-            <li><a target="_blank"  class="<?php echo ($sort_list_id === '4') ? 'active_filter' : '' ?>" data-sort-id="4" href="javascript:void(0);">4</a></li>
-            <li><a target="_blank"  class="<?php echo ($sort_list_id === '5') ? 'active_filter' : '' ?>" data-sort-id="5" href="javascript:void(0);">5</a></li>
-            <li><a target="_blank"  class="<?php echo ($sort_list_id === '6') ? 'active_filter' : '' ?>" data-sort-id="6" href="javascript:void(0);">6</a></li>
-        </ul>
+        <div class="nav-wrapper">
+            <a href="http://theringer.com" class="logo"><img src="img/logo.png" alt="The Ringer Logo" /></a>
+            <a href="javascript:void(0);" class="title">The Definitive &ldquo;Game of Thrones&rdquo; Episode Rankings</a>
+            <ul class="main-nav">
+                <li><a target="_blank" href="https://bit.ly/ringerhome">HOME</a></li>
+                <li><a target="_blank" href="https://bit.ly/ringernba">TV</a></li>
+                <li><a target="_blank" href="https://bit.ly/ringerncaabasketball">Binge Mode</a></li>
+                <li><a target="_blank" href="https://theringer.com/nba-draft">&lsquo;The Watch&rsquo; Podcast</a></li>
+                <li><a target="_blank" href="https://bit.ly/ringernbaplayoffs">Talk the Thrones</a></li>
+            </ul>
+            <ul class="fixed-nav">
+                <li><a target="_blank"  class="<?php echo ($sort_list_id === 'all') ? 'active_filter' : '' ?>" data-sort-id="all" href="javascript:void(0);">All Seasons</a></li>
+                <li><a target="_blank"  class="<?php echo ($sort_list_id === '1') ? 'active_filter' : '' ?>" data-sort-id="1" href="javascript:void(0);">1</a></li>
+                <li><a target="_blank"  class="<?php echo ($sort_list_id === '2') ? 'active_filter' : '' ?>" data-sort-id="2" href="javascript:void(0);">2</a></li>
+                <li><a target="_blank"  class="<?php echo ($sort_list_id === '3') ? 'active_filter' : '' ?>" data-sort-id="3" href="javascript:void(0);">3</a></li>
+                <li><a target="_blank"  class="<?php echo ($sort_list_id === '4') ? 'active_filter' : '' ?>" data-sort-id="4" href="javascript:void(0);">4</a></li>
+                <li><a target="_blank"  class="<?php echo ($sort_list_id === '5') ? 'active_filter' : '' ?>" data-sort-id="5" href="javascript:void(0);">5</a></li>
+                <li><a target="_blank"  class="<?php echo ($sort_list_id === '6') ? 'active_filter' : '' ?>" data-sort-id="6" href="javascript:void(0);">6</a></li>
+            </ul>
+        </div>
     </nav>
     <div class="heading-wrapper">
         <h1>
