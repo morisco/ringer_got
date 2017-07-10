c50e6b6deb176776dd77278290a71a9667667e65
Mike Morisco <mikemorisco@gmail.com>
2017-07-10 08:46:04 -0400

    Tweakin

--- components/intro.php
+++ components/intro.php
@@ -1,6 +1,6 @@
 <div id="intro">
     <div class="intro-wrapper">
-        <p>Welcome, lords, knights, ladies, and wights to <i class="regular">The Ringer</i>&rsquo;<i>s</i> definitive <i class="regular">Game of Thrones</i> episode ranking. From &ldquo;Hardhome&rdquo; and &ldquo;Blackwater&rdquo; to &ldquo;The Night Lands&rdquo; and &ldquo;First of His Name,&rdquo; our staff is breaking down each of the show’s first 60 episodes in preparation for the Season 7 premiere on July 16. The ranking is below, and yes, we are prepared to argue about it.</p>
+        <p>Welcome, lords, knights, ladies, and wights to <i class="regular">The Ringer</i>&rsquo;<i>s</i> definitive <i class="regular">Game of Thrones</i> episode ranking. From &ldquo;Hardhome&rdquo; and &ldquo;Blackwater&rdquo; to &ldquo;The Night Lands&rdquo; and &ldquo;First of His Name,&rdquo; our staff is breaking down each of the show’s first 60 episodes in preparation for the Season 7 premiere on July 16 on HBO. The ranking is below, and yes, we are prepared to argue about it.</p>
         <br/>
         <p>For a far more definitive explanation of the first 60 episodes, listen to our <a href="https://theringer.com/binge-mode-c4ef7677c662" target="_blank"><i>Binge Mode</i> podcast</a>, with Mallory Rubin and Jason Concepcion. The Mother of Dragons and Maester’s episode winners for each entry are listed here.</p>
     </div>
