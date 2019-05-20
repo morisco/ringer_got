e4e4ed258aecb61ff49b38f4e83aa55279dbdb78
morisco <mikemorisco@gmail.com>
2019-05-20 15:03:48 -0400

    Adding build step

---
+++ php_build_command.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -eo pipefail
+
+static_markup=$(php index.php)
+
+if [[ $? -eq 0 ]]; then
+    echo $static_markup > indexpz.html
+else
+    echo "build exited with status $?, output:"
+    echo "$static_markup"
+    exit 1
+fi
