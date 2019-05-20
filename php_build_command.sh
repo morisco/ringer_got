69e68b24b2ef5081494ca3b141d5290cb36ccfc4
morisco <mikemorisco@gmail.com>
2019-05-20 15:58:30 -0400

    Fixing command

--- php_build_command.sh
+++ php_build_command.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -eo pipefail
-echo $PWD;
 static_markup=$(php index.php)
 
 if [[ $? -eq 0 ]]; then
-    echo $static_markup > indexpz.html
+    echo $static_markup > index.html
 else
     echo "build exited with status $?, output:"
     echo "$static_markup"
